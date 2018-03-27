f257053114cff7eff2143f8974f74d2f79bc65bf
Teemu <Teemu@130.232.237.104>
2018-03-27 19:04:20 +0300

    Luotu Laiva konstruktori ja mahdutko metodi

--- src/Laiva.java
+++ src/Laiva.java
@@ -1,7 +1,31 @@
-public class Laiva {
+public class Laiva  {
 	private String nimi;
 	private int pituus;
 	private int x;
 	private int y;
-	private boolean horisontal;
+	private boolean horizontal;
+
+public Laiva(String nimi, int pituus, int x, int y, boolean horizontal) {
+	nimi=nimi;
+	pituus=pituus;
+	x=x;
+	y=y;
+	horizontal=horizontal;
+}
+
+public boolean mahdutko(int leveys, int korkeus) {
+	if(x+pituus<leveys) {
+		return true;
+	}
+	else {
+		return false;
+	}
+	if(y+korkeus<korkeus) {
+		return true;
+	}
+	else {
+		return false;
+	}
+	}
+}
 }
