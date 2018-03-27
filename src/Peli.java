99e1b48fdb52ffc40651cae3ff8807a66b1c49bf
riika <riika@130.232.61.74>
2018-03-27 20:30:51 +0300

    Peli luokan muokkausta

--- src/Peli.java
+++ src/Peli.java
@@ -5,18 +5,24 @@ public class Peli {
 	public static void main(String[] args) {
 		Peli laivanupotus=new Peli();
 		laivanupotus.aloitaPeli();
+		laivanupotus.lopetus();
 	}
 	private void aloitaPeli() {
 		Kentta k=new Kentta();
-		Laiva laivaL=new Lentotukialus("Titanic", );
+		Laiva laivaL=new Lentotukialus("Titanic", 2,2,true );
 		k.lisaaLaiva(laivaL);
-		System.out.println("Anna koordinaatti x");
-		Scanner sc=new Scanner(System.in);
-		int kohdex=sc.nextInt();
-		System.out.println("Anna koordinaatti y");
-		int kohdey=sc.nextInt();
-		
-		k.ammu(kohdex, kohdey);
+		int ammukset=32;
+		if (ammukset>0) {
+			System.out.println("Anna koordinaatti x");
+			Scanner sc=new Scanner(System.in);
+			int kohdex=sc.nextInt();
+			System.out.println("Anna koordinaatti y");
+			int kohdey=sc.nextInt();
+			k.ammu(kohdex, kohdey);
+			ammukset=ammukset-1;
+		}
 	}
-
+	private void lopetus() {
+		
+	}
 }
